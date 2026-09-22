b7ecc95fb7fc92cbdb2d82dcefb87b110fccdbfa
heathmickelson <heathmickelson42@gmail.com>
2026-09-22 13:41:11 -0500

    Added joke deletion

--- jokes/views.py
+++ jokes/views.py
@@ -1,5 +1,6 @@
-from django.views.generic import DetailView, ListView, CreateView, UpdateView
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 from .models import Joke
+from django.urls import reverse_lazy
 
 # Create your views here.
 class JokeListView(ListView):
@@ -17,3 +18,7 @@ class JokeCreateView(CreateView):
 class JokeUpdateView(UpdateView):
     model = Joke
     fields = ['question', 'answer']
+
+class JokeDeleteView(DeleteView):
+    model = Joke
+    success_url = reverse_lazy('jokes:list')
