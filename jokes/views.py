3be0edfd8f4b51d61a8750be56e62e76f136de1b
heathmickelson <heathmickelson42@gmail.com>
2026-09-22 13:37:25 -0500

    Expanding jokes information and added joke submission.

--- jokes/views.py
+++ jokes/views.py
@@ -1,11 +1,19 @@
-from django.views.generic import DetailView, ListView
+from django.views.generic import DetailView, ListView, CreateView, UpdateView
 from .models import Joke
 
 # Create your views here.
 class JokeListView(ListView):
     model = Joke
+    fields = ['question', 'answer']
 
 class JokeDetailView(DetailView):
     model = Joke
+    fields = ['question', 'answer']
 
+class JokeCreateView(CreateView):
+    model = Joke
+    fields = ['question', 'answer']
 
+class JokeUpdateView(UpdateView):
+    model = Joke
+    fields = ['question', 'answer']
