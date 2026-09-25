c753299d4c3757bacdfe3a5b13c93a873a3212d9
heathmickelson <heathmickelson42@gmail.com>
2026-09-25 10:49:10 -0500

    Testing slugs, more slug functionality

--- jokes/models.py
+++ jokes/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.urls import reverse
-
-
-
+from common.utils.text import unique_slug
 
 # Create your models here.
 class Joke(models.Model):
@@ -10,9 +8,18 @@ class Joke(models.Model):
     answer = models.TextField(max_length=100, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-
+    slug = models.SlugField(
+        max_length=50, unique=True, null=False, editable=False
+    )
     def get_absolute_url(self):
-        return reverse('jokes:detail', args=[str(self.pk)])
+        return reverse('jokes:detail', args=[str(self.slug)])
 
     def __str__(self):
         return self.question
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            value = str(self)
+            self.slug = unique_slug(value, type(self))
+
+        super().save(*args, **kwargs)
